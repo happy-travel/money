d3588174475a29cf43e9482f646b400035dd0650
Oleg Konovalov <oleg.konovalov@happytravel.com>
2020-06-29 12:10:24 +0300

    Add truncate method

--- HappyTravel.Money/Helpers/MoneyRound.cs
+++ HappyTravel.Money/Helpers/MoneyRound.cs
@@ -5,7 +5,7 @@ using HappyTravel.Money.Models;
 
 namespace HappyTravel.Money.Helpers
 {
-    public static class MoneyCeiler
+    public static class MoneyRound
     {
         public static MoneyAmount Ceil(MoneyAmount moneyAmount) 
             => new MoneyAmount(Ceil(moneyAmount.Amount, moneyAmount.Currency), moneyAmount.Currency);
@@ -16,6 +16,18 @@ namespace HappyTravel.Money.Helpers
         
         public static decimal Ceil(decimal target, Currencies currency) 
             => Math.Round(target, CurrencyDecimalDigits[currency], MidpointRounding.AwayFromZero);
+        
+        
+        public static MoneyAmount Truncate(MoneyAmount moneyAmount) 
+            => new MoneyAmount(Truncate(moneyAmount.Amount, moneyAmount.Currency), moneyAmount.Currency);
+        
+        
+        public static decimal Truncate(decimal target, string currency) 
+            => Truncate(target, Enum.Parse<Currencies>(currency));
+        
+        
+        public static decimal Truncate(decimal target, Currencies currency) 
+            => Math.Round(target, CurrencyDecimalDigits[currency], MidpointRounding.ToZero);
         
         
         private static readonly Dictionary<Currencies, int> CurrencyDecimalDigits = new Dictionary<Currencies, int>
