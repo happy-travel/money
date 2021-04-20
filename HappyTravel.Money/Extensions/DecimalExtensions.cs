2478b3ed25d8c77c481d49e1e9ca87526d1681c3
Kirill Taran <kirill.taran@hotmail.com>
2021-04-20 14:31:31 +0300

    An extension method added to convert decimals to MoneyAmount outside a counstructor easier

---
+++ HappyTravel.Money/Extensions/DecimalExtensions.cs
@@ -0,0 +1,15 @@
+﻿using HappyTravel.Money.Enums;
+using HappyTravel.Money.Models;
+
+namespace HappyTravel.Money.Extensions
+{
+    public static class DecimalExtensions
+    {
+        public static MoneyAmount ToMoneyAmount(this decimal target, in Currencies targetCurrency) 
+            => new(target, targetCurrency);
+
+
+        public static MoneyAmount ToMoneyAmount(this decimal target, in MoneyAmount targetAmount) 
+            => new(target, targetAmount.Currency);
+    }
+}
